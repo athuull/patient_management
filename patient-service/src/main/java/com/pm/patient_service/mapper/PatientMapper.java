e11fb5a1696fc2d585548b3f4774b60775658e59
athuull <athulvanil03@gmail.com>
2025-08-01 22:42:34 +0530

    added GET/POST on /patients

---
+++ patient-service/src/main/java/com/pm/patient_service/mapper/PatientMapper.java
@@ -0,0 +1,31 @@
+package com.pm.patient_service.mapper;
+
+import com.pm.patient_service.dto.PatientRequestDTO;
+import com.pm.patient_service.dto.PatientResponseDTO;
+import com.pm.patient_service.model.Patient;
+import org.springframework.cglib.core.Local;
+
+import java.time.LocalDate;
+
+public class PatientMapper {
+
+    public static PatientResponseDTO toDTO(Patient patient) {
+        PatientResponseDTO patientDTO  = new PatientResponseDTO();
+        patientDTO.setId(patient.getId().toString());
+        patientDTO.setName(patient.getName().toString());
+        patientDTO.setAddress(patient.getAddress().toString());
+        patientDTO.setEmail(patient.getEmail().toString());
+        patientDTO.setDateOfBirth(patient.getDateOfBirth().toString());
+        return patientDTO;
+    }
+
+    public static Patient toModel(PatientRequestDTO patientRequestDTO) {
+        Patient patient = new Patient();
+        patient.setName(patientRequestDTO.getName());
+        patient.setAddress(patientRequestDTO.getAddress());
+        patient.setEmail(patientRequestDTO.getEmail());
+        patient.setDateOfBirth(LocalDate.parse(patientRequestDTO.getDateOfBirth()));
+        patient.setRegisterDate(LocalDate.parse(patientRequestDTO.getRegisteredDate()));
+        return patient;
+    }
+}
